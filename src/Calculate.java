0642b05dda544efbcb607284cc75e8507b5321de
AbhilashTw <abhilasv@thoughtworks.com>
2013-03-15 12:09:44 +0530

    Initial Commit

---
+++ src/Calculate.java
@@ -0,0 +1,13 @@
+public class Calculate {
+    private final int firstNumber;
+    private final int secondNumber;
+
+    public Calculate(int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public int sum() {
+        return secondNumber + firstNumber;
+    }
+}
---
+++ test/CalculateTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculateTest {
+    @Test
+    public void one_plus_one_is_two() {
+        Calculate calculate = new Calculate(1, 1);
+        Assert.assertEquals(2, calculate.sum());
+    }
+
+    @Test
+    public void two_plus_one_is_three() {
+        Calculate calculate = new Calculate(2, 1);
+        Assert.assertEquals(3, calculate.sum());
+    }
+
+    @Test
+    public void zero_plus_zero_is_zero() {
+        Calculate calculate = new Calculate(0, 0);
+        Assert.assertEquals(0, calculate.sum());
+    }
+}
