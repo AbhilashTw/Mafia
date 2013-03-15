cf26e5d21e26e5120426e95d1b0ba94fc077943f
AbhilashTw <abhilasv@thoughtworks.com>
2013-03-15 12:50:20 +0530

    Again Fail

--- test/CalculateTest.java
+++ test/CalculateTest.java
@@ -33,7 +33,7 @@ public class CalculateTest {
     @Test
     public void zero_plus_ero_s_ero() {
         Calculate calculate = new Calculate(1,0);
-        Assert.assertEquals(0, calculate.sum());
+        Assert.assertEquals(5, calculate.sum());
     }
 }
 
